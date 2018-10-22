7e39afa7f4e6d61cb3991c09a09045f6c09c269a
Pekka Neuvonen <>
2018-10-22 14:22:17 +0300

    added storage

--- electron/iRath-electron-react-typescript-boilerplate-master/app/routes.tsx
+++ electron/iRath-electron-react-typescript-boilerplate-master/app/routes.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import StoragePage from './containers/StoragePage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/counter" component={CounterPage} />
+      <Route path="/storage" component={StoragePage} />
       <Route path="/" component={HomePage} />
     </Switch>
   </App>
