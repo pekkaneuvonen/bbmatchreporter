be2bcce7c42deb4af6a66816737e174d5ed6fa42
Pekka Neuvonen <>
2018-10-24 18:57:51 +0300

    added the iRath master, removed NeDB and RxDB tests

--- electron/iRath-e-r-ts-boilerplate-NeDB/src/Renderer/containers/StoragePage.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import Storage from '../components/Storage';
-
-export default class StoragePage extends React.Component<{}> {
-  render() {
-    return <Storage />;
-  }
-}
