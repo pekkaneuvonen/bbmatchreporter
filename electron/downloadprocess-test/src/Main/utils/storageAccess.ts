67cab60d988abfed5a3c1793b9c5c464c965c287
Pekka Neuvonen <>
2018-10-26 18:46:11 +0300

    Asset download test app created

---
+++ electron/downloadprocess-test/src/Main/utils/storageAccess.ts
@@ -0,0 +1,6 @@
+export default class StorageAccess {
+  init() {
+    const storageDir = "local appdata / userdata path not set";
+    console.log("StorageAccess init called [" + storageDir + "]");
+  }
+}
