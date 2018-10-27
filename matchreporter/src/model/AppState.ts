0d5aacbc7f634bc8884109687e9c452c1b7a03fe
pekka <pekka@alho.net>
2018-10-27 22:19:26 +0300

    Match screen functionality completed (until otherwise proven)

--- matchreporter/src/model/AppState.ts
+++ matchreporter/src/model/AppState.ts
@@ -1,5 +1,7 @@
 import { observable } from "mobx";
 import { Kvalue } from "../types/Kvalue";
+import { Casualty } from "./Casualty";
+import { Player } from "./Player";
 import { Report } from "./Report";
 import { Team } from "./Team";
 
@@ -22,13 +24,113 @@ export class AppState {
 
   @observable public inducementValueOverride: Kvalue;
   
-  @observable public windowWidth: number = 640;
   public defaultTimerValue: number = 300000; // 5 min = 5 * 60000
+  @observable public setTimerValue: number;
+  @observable public currentTimerValue: number;
+
+  @observable public eventsblocked: boolean;
+
+  @observable public windowWidth: number = 640;
 
   constructor() {
     this.screen = Screens.Home;
     this.showTabNavigator = false;
     this.showTab = false;
+  }
+
+  public addScorer(team: Team, playerNum: number): void {
+    if (this.report && this.homeTeam) {
+      if (!this.homeTeam.scorers) {
+        this.homeTeam.scorers = [];
+      }
+      let player: Player | undefined = this.homeTeam.scorers.find((pl: Player) => {
+          return pl.name === playerNum;
+      });
+      if (!player) {
+          player = new Player({name: playerNum});
+          this.homeTeam.scorers.push(player);
+      }
+      player.goals++;
+    }
+  }
+
+  public addThrower(team: Team, playerNum: number): void {
+    if (this.report && this.homeTeam) {
+      if (!this.homeTeam.completions) {
+        this.homeTeam.completions = [];
+      }
+      let player: Player | undefined = this.homeTeam.completions.find((pl: Player) => {
+        return pl.name === playerNum;
+      });
+      if (!player) {
+          player = new Player({name: playerNum});
+          this.homeTeam.completions.push(player);
+      }
+      player.completions++;
+    }
+  }
+
+  public addInjury(team: Team, playerNum: number, injury: number): void {
+    if (this.report && this.homeTeam) {
+      if (!this.homeTeam.injureds) {
+        this.homeTeam.injureds = [];
+      }
+      let player: Player | undefined = this.homeTeam.injureds.find((pl: Player) => {
+        return pl.name === playerNum;
+      });
+      if (!player) {
+          player = new Player({name: playerNum});
+          player.injuries = [];
+          this.homeTeam.injureds.push(player);
+      }
+      player.injuries.push(new Casualty({D68: injury}));
+    }
+  }
+
+  public addIntercept(team: Team, playerNum: number): void {
+    if (this.report && this.homeTeam) {
+      if (!this.homeTeam.intercepts) {
+        this.homeTeam.intercepts = [];
+      }
+      let player: Player | undefined = this.homeTeam.intercepts.find((pl: Player) => {
+        return pl.name === playerNum;
+      });
+      if (!player) {
+          player = new Player({name: playerNum});
+          this.homeTeam.intercepts.push(player);
+      }
+      player.intercepts++;
+    }
+  }
+
+  public addCasualty(team: Team, inflicterNum: number, injuredNum: number, injury: number): void {
+    if (this.report && this.homeTeam) {
+      if (!this.homeTeam.inflicters) {
+        this.homeTeam.inflicters = [];
+      }
+      let inflicter: Player | undefined = this.homeTeam.inflicters.find((pl: Player) => {
+        return pl.name === inflicterNum;
+      });
+      if (!inflicter) {
+        inflicter = new Player({name: inflicterNum});
+        inflicter.casualties = [];
+        this.homeTeam.inflicters.push(inflicter);
+      }
+      inflicter.casualties.push(new Casualty({D68: injury}));
+
+      if (!this.homeTeam.injureds) {
+        this.homeTeam.injureds = [];
+      }
+      let injured: Player | undefined = this.homeTeam.injureds.find((pl: Player) => {
+        return pl.name === injuredNum;
+      });
+      if (!injured) {
+        injured = new Player({name: injuredNum});
+        injured.injuries = [];
+        this.homeTeam.injureds.push(injured);
+      }
+      injured.injuries.push(new Casualty({D68: injury}));
+    }
   }
 }
 
